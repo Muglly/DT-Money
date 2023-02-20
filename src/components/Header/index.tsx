5f76f1e81e3ee0672851fef1261a92d01962da1b
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-20 15:25:30 -0300

    Created an accessible modal with Radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import LogoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import * as S from "./styles";
 
@@ -8,7 +9,19 @@ export function Header() {
       <S.HeaderContent>
         <img src={LogoImg} alt="DT Money" />
 
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   );
