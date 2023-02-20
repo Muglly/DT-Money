6681b0b6fa8cff7462eb4f35c99383945121d927
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-20 10:31:16 -0300

    Transaction Tables created

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -4,9 +4,37 @@ import * as S from "./styles";
 
 export function Transaction() {
   return (
-    <div>
+    <>
       <Header />
       <Sammury />
-    </div>
+
+      <S.TransactionContainer>
+        <S.TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighLight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>20/02/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <S.PriceHighLight variant="outcome">
+                  - R$ 59,00
+                </S.PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>11/02/2023</td>
+            </tr>
+          </tbody>
+        </S.TransactionTable>
+      </S.TransactionContainer>
+    </>
   );
 }
