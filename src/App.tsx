d8c225c4a0264d23ad8b53c3124b15cf28823a76
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-21 16:36:26 -0300

    Add TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { GlobalStyle } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { defaultThemes } from "./styles/theme/default";
 import { Transaction } from "./pages/transaction";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultThemes}>
-      <Transaction />
+      <TransactionsProvider>
+        <Transaction />
+      </TransactionsProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
