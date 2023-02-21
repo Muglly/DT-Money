f18b639a727f944bb1fc07d541ccbd63e5d9f769
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-21 19:17:47 -0300

    Add transaction sammury calculation

--- src/components/Sammury/index.tsx
+++ src/components/Sammury/index.tsx
@@ -1,8 +1,31 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 import * as S from "./styles";
 
 export function Sammury() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const sammury = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <S.SammuryContainer>
       <S.SummuryCard>
@@ -11,7 +34,7 @@ export function Sammury() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R${sammury.income}</strong>
       </S.SummuryCard>
 
       <S.SummuryCard>
@@ -20,7 +43,7 @@ export function Sammury() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>R${sammury.outcome}</strong>
       </S.SummuryCard>
 
       <S.SummuryCard variant="green">
@@ -29,7 +52,7 @@ export function Sammury() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>R${sammury.total}</strong>
       </S.SummuryCard>
     </S.SammuryContainer>
   );
